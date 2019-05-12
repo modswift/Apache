ad58cded6b80d880d5c841b43620a914d992a640
Helge Heß <me@helgehess.eu>
2019-05-12 14:59:01 +0200

    Update Package.swift for Swift 5 / 4.2
    Import CApache 2.x with the new setup.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,25 @@
+// swift-tools-version:4.2
+//
+//  Package.swift
+//  mod_swift
+//
+//  Created by Helge Hess on 11.05.18.
+//  Copyright © 2019 ZeeZide. All rights reserved.
+//
 import PackageDescription
 
 let package = Package(
     name: "Apache",
-
+    
+    products: [
+      .library(name: "Apache", targets: [ "Apache" ]),
+    ],
+    
     dependencies: [
-      .Package(url: "https://github.com/modswift/CApache.git", 
-               majorVersion: 1, minor: 0)
+      .package(url: "https://github.com/modswift/CApache.git", from: "2.0.1")
+    ],
+    
+    targets: [
+      .target(name: "Apache", dependencies: [ "CApache" ])
     ]
 )
